1b1423da54ddb5e83e34cc1cc9a508486fad0963
Achmad Yazid Fardin <ahmadyazid579@gmail.com>
2026-08-05 04:01:46 +0700

    tes cicd2

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,7 +11,7 @@ Navbar
 
         <a href="/" class="text-3xl font-black tracking-widest">
 
-            BELANDA
+            VISPA
 
         </a>
 
@@ -74,7 +74,7 @@ data-aos="fade-right"
 
 class="inline-flex rounded-full border border-cyan-400/40 bg-cyan-500/10 px-5 py-2 text-sm">
 
-Vispa Signature Collection
+Vispa Super Collection
 
 </span>
 
